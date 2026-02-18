c2978f18daf17f58fca48563acfe024325e4d902
manavagarwal-1216 <manavagrawal12@gmail.com>
2026-02-18 21:13:06 +0530

    all files pushed for test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello! App Updated Automatically!"
+    return "return AUTO DEPLOY WORKING PERFECTLY 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
